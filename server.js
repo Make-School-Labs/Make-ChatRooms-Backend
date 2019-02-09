13b66978e72b883c79a2552f29a0752c332e2300
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-08 17:10:42 -0800

    Added event listener for when user passes in username

--- server.js
+++ server.js
@@ -14,11 +14,27 @@ io.on('connection', function (socket) {
         // console.log("SOCKET ID " + socket.username + " " + socket.id)
         // console.log("Incoming Message " + message)
         console.log("Incoming Message -> ", message)
-        console.log("Message sent from -> ( ", socket.username, " ", socket.id, ")") 
+        console.log("Message sent from -> ( ", socket.username, " ", socket.id, ")")
         parsedMessage = JSON.parse(message) // Converts message JSON string into a JSON Object
         socket.broadcast.to(parsedMessage.roomOriginName).emit('chat message', message) // Broadcasts message to everyone in the room that the message was sent from except the sender
     });
 
+    // Listening for when the client sends in a username for the given socket connection!
+    socket.on("socketUsername", function (username) {
+        socket.nickname = username // Assigning the socket nickname to be the username that the client passes
+
+        // Checking if username is already present
+        if (localStorage.getItem(username)) {
+            console.log("Someone currently connected to the server shares the same username!")
+            socket.emit("usernameCollision", username)
+
+        } else { // If we don't see the username
+            localStorage.setItem(username, socket.id) // saving the item in local storage
+
+            // Emit that the username chosen because it is a successful username
+            socket.emit("validUsername", username)
+        }
+    });
 
 
 });
@@ -29,6 +45,6 @@ function getKeyByValue(object, value) { // Helper function to find username give
 }
 
 
-http.listen(4000, function () {  // Begin listening on the port 4000 when the server is ran!
+http.listen(4000, function () { // Begin listening on the port 4000 when the server is ran!
     console.log("Listening on port 4000")
 });
